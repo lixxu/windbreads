15888ae0fb348a61fbbd92d5a9e272ca8c69bab2
lix <lix_xu@jabil.com>
2016-02-23 11:02:39 +0800

    encode/decode when encrypt/decrypt

--- windbreads/security.py
+++ windbreads/security.py
@@ -5,9 +5,16 @@ from __future__ import unicode_literals
 from Crypto.Cipher import AES
 
 
-def encrypt(text, key, iv):
+def encrypt(text, key, iv, encoding='utf-8'):
+    if isinstance(text, unicode):
+        text = text.encode(encoding, 'ignore')
+
     return AES.new(key, AES.MODE_CFB, iv).encrypt(text)
 
 
-def decrypt(text, key, iv):
-    return AES.new(key, AES.MODE_CFB, iv).decrypt(text)
+def decrypt(text, key, iv, encoding='utf-8'):
+    text = AES.new(key, AES.MODE_CFB, iv).decrypt(text)
+    if not isinstance(text, unicode):
+        text = text.decode('utf-8', 'ignore')
+
+    return text

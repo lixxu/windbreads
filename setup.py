de48a0813ba3e90081af31af54cd7046817278a4
Lix Xu <xuzenglin@gmail.com>
2019-02-19 18:36:22 +0800

    update setup file

--- setup.py
+++ setup.py
@@ -7,15 +7,24 @@ windbreads
 
 Small handy snippets of non-GUI python
 """
+import os.path
 from setuptools import setup
-import windbreads
+
+folder = os.path.dirname(os.path.abspath(__file__))
+with open(os.path.join(folder, "windbreads/__init__.py")) as f:
+    for line in f:
+        if line.startswith("__version__ = "):
+            version = line.split("=")[-1].strip().replace('"', "")
+        elif line.startswith("__author__ = "):
+            author = line.split("=")[-1].strip().replace('"', "")
+            break
 
 setup(
     name='windbreads',
-    version=windbreads.__version__,
+    version=version.replace("'", ""),
     url='https://github.com/lixxu/windbreads',
     license='BSD',
-    author=windbreads.__author__,
+    author=author.replace("'", ""),
     author_email='xuzenglin@gmail.com',
     description='Small handy snippets of non-GUI python',
     long_description=__doc__,
@@ -29,6 +38,8 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules'
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
     ]
 )
